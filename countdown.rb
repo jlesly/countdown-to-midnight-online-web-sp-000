51cb330aa9980d9fe9b7878628b9776ff5473792
Lesly Juarez <leslyj2@gmail.com>
2020-03-30 13:31:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+while num > 0
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+end
+  "HAPPY NEW YEAR!"
+end	end
